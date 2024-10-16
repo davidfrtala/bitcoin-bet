f24449f945bccd3eea28de1f494fb1a171ab534c
David Frtala <david.frtala@gmail.com>
2024-10-16 06:07:10 +0200

    fix: calculate-result lambda got broken calculation

--- apps/backend/lambda/calculate-result/index.js
+++ apps/backend/lambda/calculate-result/index.js
@@ -1,27 +1,27 @@
 exports.handler = async (event) => {
   const { player, userBet, btcPriceStart, btcPriceEnd } = event;
-  const { guess } = userBet.item;
+  const { S: guess } = userBet.item.guess;
 
   const startPrice = parseFloat(btcPriceStart.value);
   const endPrice = parseFloat(btcPriceEnd.value);
   const score = parseInt(player.score);
-  const priceDiff = (endPrice - startPrice).toFixed(2);
+  const priceDiff = endPrice - startPrice;
 
   let result;
   if (
-    (guess === "UP" && priceDiff > 0) ||
-    (guess === "DOWN" && priceDiff < 0)
+    (guess === 'UP' && priceDiff > 0) ||
+    (guess === 'DOWN' && priceDiff < 0)
   ) {
-    result = "WIN";
+    result = 'WIN';
   } else {
-    result = "LOSS";
+    result = 'LOSS';
   }
 
-  const scoreChange = score + (result === "WIN" ? 1 : -1);
+  const scoreChange = score + (result === 'WIN' ? 1 : -1);
 
   return {
     result,
-    priceDiff,
+    priceDiff: priceDiff.toFixed(2),
     newScore: scoreChange.toString(),
   };
 };
